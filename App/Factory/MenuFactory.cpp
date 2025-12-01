5757673d8f423e5e9149d38cc9e62f85996feb88
Driller34 <dragonhack123@op.pl>
2025-12-01 23:50:58 +0100

    menu factory code style improve

--- App/Factory/MenuFactory.cpp
+++ App/Factory/MenuFactory.cpp
@@ -2,98 +2,98 @@
 
 namespace factory
 {
-    std::unique_ptr<MenuLayer> createMenu(LayerManager& layerManager,
+std::unique_ptr<MenuLayer> createMenu(LayerManager& layerManager,
         ResourceManager& resourceManager)
-    {
-        auto menuLayer = std::make_unique<MenuLayer>(layerManager, 
-            resourceManager.getImage("Images/MenuBackground.bmp"),
-            Vec2i{35, 35});
+{
+    auto menuLayer = std::make_unique<MenuLayer>(layerManager, 
+        resourceManager.getImage("Images/MenuBackground.bmp"),
+        Vec2i{35, 35});
 
-        menuLayer->addOption(gui::Option{
-            resourceManager.getImage("Images/start.bmp"),
-            Color{0, 255, 0, 255},
-            [&](){
-                layerManager.push(createDifficultMenu(layerManager, resourceManager));
-        }});
+    menuLayer->addOption(gui::Option{
+        resourceManager.getImage("Images/start.bmp"),
+        Color{0, 255, 0, 255},
+        [&](){
+            layerManager.push(createDifficultMenu(layerManager, resourceManager));
+    }});
 
-        menuLayer->addOption(gui::Option{
-            resourceManager.getImage("Images/exit.bmp"),
-            Color{0, 255, 0, 255},
-            [&](){
-                layerManager.pop();
-        }});
+    menuLayer->addOption(gui::Option{
+        resourceManager.getImage("Images/exit.bmp"),
+        Color{0, 255, 0, 255},
+        [&](){
+            layerManager.pop();
+    }});
 
-        return menuLayer;
-    }
+    return menuLayer;
+}
 
-    std::unique_ptr<MenuLayer> createDifficultMenu(LayerManager& layerManager,
-        ResourceManager& resourceManager)
-    {
-        auto menuLayer = std::make_unique<MenuLayer>(layerManager, 
-            resourceManager.getImage("Images/menuDifficultyBackground.bmp"),
-            Vec2i{35, 25});
+std::unique_ptr<MenuLayer> createDifficultMenu(LayerManager& layerManager,
+    ResourceManager& resourceManager)
+{
+    auto menuLayer = std::make_unique<MenuLayer>(layerManager, 
+        resourceManager.getImage("Images/menuDifficultyBackground.bmp"),
+        Vec2i{35, 25});
 
-        menuLayer->addOption(gui::Option{
-            resourceManager.getImage("Images/easyOption.bmp"),
-            Color{0, 255, 0, 255},
-            [&](){
-                layerManager.push(std::make_unique<GameLayer>(layerManager, 
-                    resourceManager,
-                    factory::createEasySettings()
-                ));
-        }});
+    menuLayer->addOption(gui::Option{
+        resourceManager.getImage("Images/easyOption.bmp"),
+        Color{0, 255, 0, 255},
+        [&](){
+            layerManager.push(std::make_unique<GameLayer>(layerManager, 
+                resourceManager,
+                factory::createEasySettings()
+            ));
+    }});
 
-        menuLayer->addOption(gui::Option{
-            resourceManager.getImage("Images/mediumOption.bmp"),
-            Color{0, 255, 0, 255},
-            [&](){
-                layerManager.push(std::make_unique<GameLayer>(layerManager, 
-                    resourceManager,
-                    factory::createMediumSettings()
-                ));
-        }});
+    menuLayer->addOption(gui::Option{
+        resourceManager.getImage("Images/mediumOption.bmp"),
+        Color{0, 255, 0, 255},
+        [&](){
+            layerManager.push(std::make_unique<GameLayer>(layerManager, 
+                resourceManager,
+                factory::createMediumSettings()
+            ));
+    }});
 
-        menuLayer->addOption(gui::Option{
-            resourceManager.getImage("Images/hardOption.bmp"),
-            Color{0, 255, 0, 255},
-            [&](){
-                layerManager.push(std::make_unique<GameLayer>(layerManager, 
-                    resourceManager,
-                    factory::createHardSettings()
-                ));
-        }});
+    menuLayer->addOption(gui::Option{
+        resourceManager.getImage("Images/hardOption.bmp"),
+        Color{0, 255, 0, 255},
+        [&](){
+            layerManager.push(std::make_unique<GameLayer>(layerManager, 
+                resourceManager,
+                factory::createHardSettings()
+            ));
+    }});
 
-        menuLayer->addOption(gui::Option{
-            resourceManager.getImage("Images/exit.bmp"),
-            Color{0, 255, 0, 255},
-            [&](){
-                layerManager.pop();
-        }});
+    menuLayer->addOption(gui::Option{
+        resourceManager.getImage("Images/exit.bmp"),
+        Color{0, 255, 0, 255},
+        [&](){
+            layerManager.pop();
+    }});
 
-        return menuLayer;
-    }
+    return menuLayer;
+}
 
-    std::unique_ptr<MenuLayer> createPauseMenu(LayerManager& layerManager,
-        ResourceManager& resourceManager)
-    {
-        auto menuLayer = std::make_unique<MenuLayer>(layerManager, 
-            resourceManager.getImage("Images/pauseBackground.bmp"),
-            Vec2i{35, 25});
+std::unique_ptr<MenuLayer> createPauseMenu(LayerManager& layerManager,
+    ResourceManager& resourceManager)
+{
+    auto menuLayer = std::make_unique<MenuLayer>(layerManager, 
+        resourceManager.getImage("Images/pauseBackground.bmp"),
+        Vec2i{35, 25});
 
-        menuLayer->addOption(gui::Option{
-            resourceManager.getImage("Images/resumeOption.bmp"),
-            Color{0, 255, 0, 255},
-            [&](){
-                layerManager.pop();
-        }});
+    menuLayer->addOption(gui::Option{
+        resourceManager.getImage("Images/resumeOption.bmp"),
+        Color{0, 255, 0, 255},
+        [&](){
+            layerManager.pop();
+    }});
 
-        menuLayer->addOption(gui::Option{
-            resourceManager.getImage("Images/exit.bmp"),
-            Color{0, 255, 0, 255},
-            [&](){
-                layerManager.pop(3ULL);
-        }});
+    menuLayer->addOption(gui::Option{
+        resourceManager.getImage("Images/exit.bmp"),
+        Color{0, 255, 0, 255},
+        [&](){
+            layerManager.pop(3ULL);
+    }});
 
-        return menuLayer;
-    }
+    return menuLayer;
+}
 }
